99ee5d9c515534b421571a25081327cfca6359ec
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-08 15:44:09 -0700

    Add application assembly marker

---
+++ src/ProjectHopeLynden.Application/ApplicationAssembly.cs
@@ -0,0 +1,8 @@
+using ProjectHopeLynden.Domain;
+
+namespace ProjectHopeLynden.Application;
+
+public static class ApplicationAssembly
+{
+    public static Type DomainMarkerType => typeof(DomainAssembly);
+}
