193b9e60783c0b3239c4ed494a56f2b604045754
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-08 21:54:53 -0700

    Issue #8: Add initial inventory seed constants

---
+++ src/ProjectHopeLynden.Infrastructure/Persistence/Seeding/InitialInventorySeedData.cs
@@ -0,0 +1,22 @@
+namespace ProjectHopeLynden.Infrastructure.Persistence.Seeding;
+
+public static class InitialInventorySeedData
+{
+    public static readonly string[] CategoryNames =
+    [
+        "Canned Vegetables",
+        "Canned Beans",
+        "Tomatoes",
+        "Canned Meat",
+        "Snacks",
+        "Cereals",
+    ];
+
+    public static readonly string[] LocationNames =
+    [
+        "Shelf",
+        "Back Room",
+        "Freezer",
+        "Pantry Area",
+    ];
+}
