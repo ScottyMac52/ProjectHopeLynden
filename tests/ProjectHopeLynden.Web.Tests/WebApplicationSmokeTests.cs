5b053e078f6fd8a46be58c033ab7e1cc36570af5
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-08 22:40:24 -0700

    Issue #27: Use hosting environment constants in web smoke test

--- tests/ProjectHopeLynden.Web.Tests/WebApplicationSmokeTests.cs
+++ tests/ProjectHopeLynden.Web.Tests/WebApplicationSmokeTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Xunit;
 
 namespace ProjectHopeLynden.Web.Tests;
