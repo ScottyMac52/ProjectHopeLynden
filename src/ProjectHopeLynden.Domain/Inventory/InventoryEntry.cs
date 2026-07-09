7d4f96787422e5472a0bfb38b91a40a93d490aee
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-08 21:29:29 -0700

    Issue #7: Add inventory entry entity

---
+++ src/ProjectHopeLynden.Domain/Inventory/InventoryEntry.cs
@@ -0,0 +1,30 @@
+namespace ProjectHopeLynden.Domain.Inventory;
+
+public sealed class InventoryEntry
+{
+    public int Id { get; set; }
+
+    public int ItemId { get; set; }
+
+    public Item Item { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; } = null!;
+
+    public int LocationId { get; set; }
+
+    public Location Location { get; set; } = null!;
+
+    public int CurrentQuantity { get; set; }
+
+    public DateTime? BestByDate { get; set; }
+
+    public bool IsCommodity { get; set; }
+
+    public bool IsMenuItem { get; set; }
+
+    public DateTime LastUpdatedAtUtc { get; set; }
+
+    public List<InventoryCountHistory> CountHistory { get; set; } = [];
+}
