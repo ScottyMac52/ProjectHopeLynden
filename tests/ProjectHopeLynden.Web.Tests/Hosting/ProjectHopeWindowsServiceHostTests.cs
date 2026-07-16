cd8fa3e55fda2573b05f3d2489feec36f59b4ff9
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-16 10:06:09 -0700

    Issue #70: Test Windows service host configuration [skip ci]

---
+++ tests/ProjectHopeLynden.Web.Tests/Hosting/ProjectHopeWindowsServiceHostTests.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.Hosting;
+using ProjectHopeLynden.Web.Hosting;
+using Xunit;
+
+namespace ProjectHopeLynden.Web.Tests.Hosting;
+
+public sealed class ProjectHopeWindowsServiceHostTests
+{
+    [Fact]
+    public void ServiceContract_UsesStableNames()
+    {
+        Assert.Equal("ProjectHopeLynden", ProjectHopeWindowsServiceHost.ServiceName);
+        Assert.Equal(
+            "Project Hope Lynden Inventory Server",
+            ProjectHopeWindowsServiceHost.DisplayName);
+    }
+
+    [Fact]
+    public void ConfigureWindowsServiceOptions_AssignsServiceName()
+    {
+        var options = new WindowsServiceLifetimeOptions();
+
+        ProjectHopeWindowsServiceHost.ConfigureWindowsServiceOptions(options);
+
+        Assert.Equal(ProjectHopeWindowsServiceHost.ServiceName, options.ServiceName);
+    }
+
+    [Fact]
+    public void ConfigureWindowsServiceOptions_RejectsNullOptions()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+            ProjectHopeWindowsServiceHost.ConfigureWindowsServiceOptions(null!));
+    }
+
+    [Fact]
+    public void Configure_AcceptsNormalInteractiveHost()
+    {
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+        {
+            EnvironmentName = Environments.Development,
+        });
+
+        ProjectHopeWindowsServiceHost.Configure(builder);
+    }
+
+    [Fact]
+    public void Configure_RejectsNullBuilder()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+            ProjectHopeWindowsServiceHost.Configure(null!));
+    }
+}
