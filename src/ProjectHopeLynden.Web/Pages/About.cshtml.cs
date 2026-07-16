f9f777e4a37a9593258626c9f36d0ca627ad0316
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-16 13:41:12 -0700

    Issue #72: Add application About page model [skip ci]

---
+++ src/ProjectHopeLynden.Web/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectHopeLynden.Web.Information;
+
+namespace ProjectHopeLynden.Web.Pages;
+
+public sealed class AboutModel : PageModel
+{
+    public string ApplicationName { get; } = "Project Hope Inventory";
+
+    public string Version { get; } = ApplicationVersionFormatter.Format(
+        typeof(Program).Assembly.GetName().Version);
+
+    public string OrganizationName { get; } = "Project Hope Food Bank of Lynden";
+
+    public string CreatorName { get; } = "Scott McIntosh";
+}
