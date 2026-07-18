9d47493adf0125b945fbee7e037e456c956589d9
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-18 11:34:23 -0700

    Issue #79: Define incoming order service contract [skip ci]

---
+++ src/ProjectHopeLynden.Application/IncomingOrders/IIncomingOrderService.cs
@@ -0,0 +1,32 @@
+namespace ProjectHopeLynden.Application.IncomingOrders;
+
+public interface IIncomingOrderService
+{
+    Task<IncomingOrdersView> GetOrdersAsync(CancellationToken cancellationToken = default);
+
+    Task<IncomingOrderOperationResult> CreateAsync(
+        IncomingOrderSaveRequest request,
+        DateTime createdAtUtc,
+        CancellationToken cancellationToken = default);
+
+    Task<IncomingOrderOperationResult> UpdateAsync(
+        int incomingOrderId,
+        IncomingOrderSaveRequest request,
+        DateTime updatedAtUtc,
+        CancellationToken cancellationToken = default);
+
+    Task<IncomingOrderOperationResult> CancelAsync(
+        int incomingOrderId,
+        DateTime cancelledAtUtc,
+        CancellationToken cancellationToken = default);
+
+    Task<IncomingOrderOperationResult> ReceiveAsync(
+        int incomingOrderId,
+        DateTime receivedAtUtc,
+        CancellationToken cancellationToken = default);
+
+    Task<IncomingOrderProcessingResult> ReceiveDueAsync(
+        DateOnly dueThroughDate,
+        DateTime receivedAtUtc,
+        CancellationToken cancellationToken = default);
+}
